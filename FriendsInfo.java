34201588285fc573326af40e062ad7fc8a50deb9
Khushijaiswal-07 <khushibtn@gmail.com>
2026-03-22 15:44:31 +0530

    Level 2 problem 2 done

---
+++ FriendsInfo.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class FriendsInfo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Names of friends
+        String[] names = {"Amar", "Akbar", "Anthony"};
+
+        // Arrays for age and height
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        // Input
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nEnter details for " + names[i]);
+
+            System.out.print("Enter age: ");
+            age[i] = sc.nextInt();
+
+            System.out.print("Enter height (in cm): ");
+            height[i] = sc.nextDouble();
+        }
+
+        // Assume first friend is youngest and tallest
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        // Loop to find youngest and tallest
+        for (int i = 1; i < 3; i++) {
+
+            if (age[i] < age[youngestIndex]) {
+                youngestIndex = i;
+            }
+
+            if (height[i] > height[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Output
+        System.out.println("\nYoungest Friend: " + names[youngestIndex] +
+                " (Age: " + age[youngestIndex] + ")");
+
+        System.out.println("Tallest Friend: " + names[tallestIndex] +
+                " (Height: " + height[tallestIndex] + " cm)");
+
+        sc.close();
+    }
+}
