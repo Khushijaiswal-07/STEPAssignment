426bb2dacfc32c64d1a984194a226f296a6bcc42
Khushijaiswal-07 <khushibtn@gmail.com>
2026-04-11 18:36:03 +0530

    Level 1 problem 4 done

---
+++ NumberSignChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class NumberSignChecker {
+
+    public int checkNumber(int n) {
+        if (n > 0) return 1;
+        else if (n < 0) return -1;
+        else return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        NumberSignChecker obj = new NumberSignChecker();
+        int result = obj.checkNumber(n);
+
+        if (result == 1)
+            System.out.println("The number is Positive");
+        else if (result == -1)
+            System.out.println("The number is Negative");
+        else
+            System.out.println("The number is Zero");
+
+        sc.close();
+    }
+}
