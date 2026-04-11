5a706af8081234e20258ae686e51bd1ff7452cf4
Khushijaiswal-07 <khushibtn@gmail.com>
2026-04-11 18:44:54 +0530

    Level 2 problem 3 done

---
+++ LeapYearChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class LeapYearChecker {
+
+    public boolean isLeap(int year) {
+
+        if (year < 1582)
+            return false;
+
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        LeapYearChecker obj = new LeapYearChecker();
+
+        if (obj.isLeap(year))
+            System.out.println("Leap Year");
+        else
+            System.out.println("Not a Leap Year");
+
+        sc.close();
+    }
+}
