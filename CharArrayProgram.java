8c95c075832989f17ae3c6ef9cd42204f35678b2
Khushijaiswal-07 <khushibtn@gmail.com>
2026-05-01 14:39:57 +0530

    Level 1 problem 3 week9and10 completed

---
+++ CharArrayProgram.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class CharArrayProgram {
+
+    public static char[] getCharacters(String text) {
+        char[] result = new char[text.length()];
+
+        for (int i = 0; i < text.length(); i++) {
+            result[i] = text.charAt(i);
+        }
+
+        return result;
+    }
+
+    public static boolean compareArrays(char[] a, char[] b) {
+        if (a.length != b.length) return false;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter text: ");
+            String text = scanner.next();
+
+            char[] manual = getCharacters(text);
+            char[] builtin = text.toCharArray();
+
+            System.out.println("Arrays equal: " + compareArrays(manual, builtin));
+
+        } finally {
+            scanner.close();
+        }
+    }
+}
