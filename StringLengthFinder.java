e80425d340af54d52d073a17acc2eb51aaeafde8
Khushijaiswal-07 <khushibtn@gmail.com>
2026-05-01 14:43:33 +0530

    Level 2 problem 1 week9and10 completed

---
+++ StringLengthFinder.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class StringLengthFinder {
+
+    // Method to find length using exception
+    public static int findLength(String text) {
+        int count = 0;
+
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter text: ");
+            String text = sc.next();
+
+            int manualLength = findLength(text);
+            int actualLength = text.length();
+
+            System.out.println("Manual Length: " + manualLength);
+            System.out.println("Built-in Length: " + actualLength);
+
+        } finally {
+            sc.close();
+        }
+    }
+}
